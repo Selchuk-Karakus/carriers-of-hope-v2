0716cabcb0346373385893f41d7a30e02e3a2bed
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2022-07-12 07:16:28 +0100

    Update App.js
    cleanup/ routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import AllItems from "./components/AllItems";
+import ItemDetails from "./components/ItemDetails";
+import RequestForm from "./components/RequestForm";
+import Navbar from "./components/Navbar";
+import OrderSummary from "./components/OrderSummary";
+import Footer from "./components/Footer";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="all-items" element={<AllItems />}></Route>
+        <Route path="item-details/:itemId" element={<ItemDetails />}></Route>
+        <Route path="item-details" element={<ItemDetails />}></Route>
+        <Route path="request-form" element={<RequestForm />}></Route>
+        <Route path="order-summary" element={<OrderSummary />}></Route>
+        <Route path="*" element={<NoMatch />}></Route>
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
