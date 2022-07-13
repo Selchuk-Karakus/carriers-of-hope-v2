ca6e1d43a8071c7fb22683b44f4295d657a09b5a
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2022-07-13 07:48:12 +0100

    Update AllProducts.js
    add search input

--- client/src/components/AllProducts.js
+++ client/src/components/AllProducts.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function AllProducts() {
+  const navigate = useNavigate();
   return (
     <>
+      <button onClick={() => navigate(-1)}>back</button>
       <div>
         <p>All Products</p>
+        <input type="search" placeholder="search products" />
+        <Link to="/filtered-products">
+          <div>
+            <p>Filter</p>
+          </div>
+        </Link>
         <Link to="/product-details">
           {" "}
           <div>
