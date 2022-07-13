a931f8064b36c623828b9a4ffc8dcc84fdc590d5
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2022-07-13 02:58:05 +0100

    Update App.js
    path to user login

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Container } from "./components/styles/Container.styled";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import AllProducts from "./components/AllProducts";
 import ProductDetails from "./components/ProductDetails";
 import RequestForm from "./components/RequestForm";
+import UserLogin from "./components//UserLogin";
 import Navbar from "./components/Navbar";
 import OrderSummary from "./components/OrderSummary";
 import Footer from "./components/Footer";
@@ -12,20 +14,23 @@ import NoMatch from "./components/NoMatch";
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="all-products" element={<AllProducts />}></Route>
-        <Route
-          path="product-details/:itemId"
-          element={<ProductDetails />}
-        ></Route>
-        <Route path="product-details" element={<ProductDetails />}></Route>
-        <Route path="request-form" element={<RequestForm />}></Route>
-        <Route path="order-summary" element={<OrderSummary />}></Route>
-        <Route path="*" element={<NoMatch />}></Route>
-      </Routes>
-      <Footer />
+      <Container>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="all-products" element={<AllProducts />}></Route>
+          <Route
+            path="product-details/:itemId"
+            element={<ProductDetails />}
+          ></Route>
+          <Route path="product-details" element={<ProductDetails />}></Route>
+          <Route path="user-login" element={<UserLogin />} />
+          <Route path="request-form" element={<RequestForm />}></Route>
+          <Route path="order-summary" element={<OrderSummary />}></Route>
+          <Route path="*" element={<NoMatch />}></Route>
+        </Routes>
+        <Footer />
+      </Container>
     </>
   );
 }
