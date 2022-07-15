62e31fff591fe73a6706c29dcf4241fd84513d06
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-07-15 11:46:08 +0100

    Import library to help hash passwords

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Homepage from "./Pages/Homepage";
 import ProductsPage from "./Pages/ProductsPage";
 import ProductDetails from "./Pages/ProductDetails";
 import RequestForm from "./components/RequestForm";
-import UserLogin from "./components//UserLogin";
+import UserLogin from "./components/UserLogin";
 import Navbar from "./components/Navbar";
 import OrderSummary from "./components/OrderSummary";
 import Footer from "./components/Footer";
@@ -16,23 +16,22 @@ function App() {
   return (
     <Global>
       <Navbar />
+      <ProductsProvider>
       <Routes>
         <Route path="/"  exact element={<Homepage />}></Route>
-        <ProductsProvider>
         <Route path="products" exact  element={<ProductsPage />}></Route>
         <Route
           path="product-details/:name"
           exact 
           element={<ProductDetails />}
         ></Route>
-        </ProductsProvider>
         <Route path="user-login" element={<UserLogin />} />
         <Route path="request-form" exact  element={<RequestForm />}></Route>
         
         <Route path="order-summary" exact  element={<OrderSummary />}></Route>
         <Route path="*"  exact element={<NoMatch />}></Route>
       </Routes>
-    
+      </ProductsProvider>
       <Footer />
     </Global>
 
---
+++ client/src/components/UserLogin.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const UserLogin = () => {
+
+   return (
+        <div >
+            
+        </div>
+   );
+ };
+ 
+ export default UserLogin;
+ 
