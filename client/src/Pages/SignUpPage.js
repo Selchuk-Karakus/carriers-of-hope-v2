f7d0c48ae84294dfaed2c93221c18ada4e4860bd
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-07-19 19:22:47 +0100

    Add simple user interface for submission logic

--- client/src/Pages/SignUpPage.js
+++ client/src/Pages/SignUpPage.js
@@ -17,8 +17,9 @@ const SignUpPage = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
 
   // States for checking the errors for the functionality of the form
-  // const [submitted, setSubmitted] = useState(false);
-  // const [error, setError] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorConfirmPassword, setErrorConfirmPassword] = useState(false);
 
   // Handling the form submission
   const handleSubmit = (e) => {
@@ -35,10 +36,12 @@ const SignUpPage = () => {
       password === "" ||
       confirmPassword === ""
     ) {
-      // setError(true);
+      setError(true);
+    } else if (password !== confirmPassword) {
+      setErrorConfirmPassword(true);
     } else {
-      // setSubmitted(true);
-      // setError(false);
+      setSubmitted(true);
+      setError(false);
     }
 
     signUp({
@@ -55,48 +58,97 @@ const SignUpPage = () => {
     });
   };
 
+  // Showing success message
+  const successMessage = () => {
+    return (
+      <div
+        style={{
+          display: submitted ? "" : "none",
+        }}
+      >
+        <h1>User {`${first_name} ${last_name}`} successfully registered!!</h1>
+      </div>
+    );
+  };
+
+  // Showing error message if error is true
+  const errorMessage = () => {
+    return (
+      <div
+        style={{
+          display: error ? "" : "none",
+        }}
+      >
+        <h1>Please enter all the fields</h1>
+      </div>
+    );
+  };
+
+  // Showing error message if passwordconfirm error is true
+  const errorMessageConfirmPassword = () => {
+    return (
+      <div
+        style={{
+          display: errorConfirmPassword ? "" : "none",
+        }}
+      >
+        <h1>Please enter matching password for both the fields</h1>
+      </div>
+    );
+  };
+
   const navigate = useNavigate();
   return (
     <>
       <button onClick={() => navigate(-1)}>back</button>
       {/* Calling to the methods */}
-      {/* <div className="messages">
+      <div className="messages">
         {errorMessage()}
         {successMessage()}
-      </div> */}
+        {errorMessageConfirmPassword()}
+      </div>
+      <div>
+        <h1>Create an account</h1>
+      </div>
       <Form onSubmit={handleSubmit}>
         <label>First Name</label>
         <input
+          className="form-control"
           type="text"
           name="first_name"
           onChange={(e) => setFirstName(e.target.value)}
         />
         <label>Last Name</label>
         <input
+          className="form-control"
           type="text"
           name="last_name"
           onChange={(e) => setLastName(e.target.value)}
         />
         <label>Address Line 1</label>
         <input
+          className="form-control"
           type="text"
           name="address"
           onChange={(e) => setAddress(e.target.value)}
         />
-        <label>City</label>
+        <label>Town/City</label>
         <input
+          className="form-control"
           type="text"
           name="city"
           onChange={(e) => setCity(e.target.value)}
         />
-        <label>Post Code</label>
+        <label>Postcode</label>
         <input
+          className="form-control"
           type="text"
           name="postcode"
           onChange={(e) => setPostcode(e.target.value)}
         />
         <label>Country</label>
         <input
+          className="form-control"
           type="text"
           name="country"
           onChange={(e) => setCountry(e.target.value)}
@@ -109,15 +161,16 @@ const SignUpPage = () => {
           name="email"
           onChange={(e) => setEmail(e.target.value)}
         />
-        <label>Telephone</label>
+        <label>Phone Number</label>
         <input
+          className="form-control"
           type="tel"
           name="phone"
           //           pattern="^\s*\(?(020[7,8]{1}\)?[ ]?[1-9]{1}[0-9{2}[ ]?[0-9]{4})|(0[1-8]{1}[0-9]{3}\)?[ ]?[1-9]{1}[0-9]{2}[ ]?[0-9]{3})\s*$
           // "
           onChange={(e) => setTelephone(e.target.value)}
         />
-        <label>Password:</label>
+        <label>Password</label>
         <input
           type="password"
           className="form-control"
@@ -125,7 +178,7 @@ const SignUpPage = () => {
           name="password"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <label>Retype Password:</label>
+        <label>Retype Password</label>
         <input
           type="password"
           className="form-control"
