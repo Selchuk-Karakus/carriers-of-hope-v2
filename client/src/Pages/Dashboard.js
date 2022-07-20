5448279f0c77135d41ba38799373bd60e075b256
mahri09 <mahriaa88@gmail.com>
2022-07-20 17:19:40 +0100

    fixed error from merge

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -1,22 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
 import ProductsTable from '../components/ProductsTable'
-
-
-function Dashboard (){
-
-
-
-   return(
-      <div>
-         <h2>Admin name</h2>
-         <button>Orders</button>
-         <button>Products</button>
-         <button>Customers</button>
-      <ProductsTable/>
-      </div>
-   )
-=======
 import OrdersTable from '../components/OrdersTable';
 
 function Dashboard() {
@@ -27,10 +10,10 @@ function Dashboard() {
        <button>Orders</button>
        <button>Products</button>
        <button>Customers</button>
+       <ProductsTable/>
        <OrdersTable />
      </div>
    );
->>>>>>> 183e9315aa375d6e8e808b055064877279090b0d
 }
 
 export default Dashboard
