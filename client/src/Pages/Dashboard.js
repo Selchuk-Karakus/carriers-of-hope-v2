ce85a24085fef52841ecc788ed33a6e5e4ffb5ed
Hamra <hamra.khan.b@gmail.com>
2022-07-21 18:36:09 +0100

    Styling the admin dashboard for orders tab -  installed node-sass - tidied up App.js file - Added dropdown menu and other features to OrdersTable.js - Added order features to Dashboard.js - Created and added styling for dashboard page to dashboard.scss file - Removed the string surrounding variable values and added some variables to _variables.scss file

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -1,17 +1,52 @@
-import React from 'react'
-import OrdersTable from '../components/OrdersTable';
+import React from "react";
+import OrdersTable from "../components/OrdersTable";
+import "../styles/dashboard.scss";
+import { CgProfile } from "react-icons/cg";
+
 
 function Dashboard() {
-  
-   return (
-     <div>
-       <h2>Admin name</h2>
-       <button>Orders</button>
-       <button>Products</button>
-       <button>Customers</button>
-       <OrdersTable />
-     </div>
-   );
+  return (
+    <div className="admin-dashboard">
+      <div className="admin-navbar">
+        <div className="admin-profile">
+          <h1>Admin name</h1>
+          <span className="profile-icon">
+            <CgProfile />
+          </span>
+        </div>
+        <div className="logout">Logout</div>
+      </div>
+
+      <div className="button-container">
+        <div className="button">
+          <button className="admin-tab-button" id="orders-button">
+            Orders
+          </button>
+        </div>
+
+        <div className="button">
+          <button className="admin-tab-button" id="products-button">
+            Products
+          </button>
+        </div>
+
+        <div className="button">
+          <button className="admin-tab-button" id="customers-button">
+            Customers
+          </button>
+        </div>
+      </div>
+
+      <h2>Orders</h2>
+
+      <OrdersTable />
+
+      <div className="results">
+        <span className='show-results'>Show 10 results</span>
+        <span className="number">{'< 1 >'}</span>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
