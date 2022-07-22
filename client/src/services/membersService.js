04f9322698aa21c8120b6b47a02beef0f23a42cc
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-07-22 12:05:16 +0100

    Create: members service, add simple logic

---
+++ client/src/services/membersService.js
@@ -0,0 +1,32 @@
+// Send a default GET request to /members to GET all members
+export const getMembers = async () => {
+  return await fetch("http://localhost:8000/members")
+    .then(checkStatus)
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) =>
+      console.log(`${error}: Looks like there was a problem fetching data`)
+    );
+};
+
+// Send a default GET request to /member/:id to GET a member
+export const getMemberById = async (memberId) => {
+  return await fetch(`http://localhost:8000/members/${memberId}`)
+    .then(checkStatus)
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) =>
+      console.log(`${error}: Looks like there was a problem fetching data`)
+    );
+}
+
+// Helper function taking care of fetch response logic
+const checkStatus = (res) => {
+  if (res.ok) {
+    return Promise.resolve(res);
+  } else {
+    return Promise.reject(new Error(res.statusTex));
+  }
+};
