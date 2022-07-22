85e453c653a1344d3d138992e7f513d4655e920f
Hamra <hamra.khan.b@gmail.com>
2022-07-22 18:46:37 +0100

    Styling admin order details (incomplete) - Added product details and form in OrderDetails.js - Styling for order details

--- client/src/Pages/OrderDetails.js
+++ client/src/Pages/OrderDetails.js
@@ -2,17 +2,21 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { OrdersContext } from "../Contexts/OrdersContext";
 import { getOrders } from "../services/orderService";
+import { Link } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
+import "../styles/orderdetails.scss";
+import { CgProfile } from "react-icons/cg";
 
 function OrderDetails() {
   const { orders, setOrders } = useContext(OrdersContext);
-  const [ orderDetailsObj, setOrderDetailsObj ] = useState({});
-  
+  const [orderDetailsObj, setOrderDetailsObj] = useState({});
+
   const { id } = useParams();
   useEffect(() => {
-    if(orders.length === 0 ) {
-        getOrders().then((orders) => {
-            setOrders(orders);
-        });
+    if (orders.length === 0) {
+      getOrders().then((orders) => {
+        setOrders(orders);
+      });
     }
     const res = orders.find((orderVal) => orderVal.id === Number(id));
 
@@ -20,30 +24,86 @@ function OrderDetails() {
   }, [orders, id, setOrderDetailsObj, setOrders]);
   return (
     <>
-      <div>Order details</div>
-      <div>
-        <div>
-          <img src="https://www.facebook.com/randomimagesbr" alt="random"/>
+      <div className="admin-navbar">
+        <div className="admin-profile">
+          <h1>Admin name</h1>
+          <span className="profile-icon">
+            <CgProfile />
+          </span>
         </div>
-        <div>
-          <span>Product Name: {orderDetailsObj?.products?.name}</span>
-          <span>Product ID: {orderDetailsObj?.products?.id}</span>
+        <div className="logout">Logout</div>
+      </div>
+
+      <Link to={"/admin/dashboard"}>
+        <button className="back-button">
+          <span className="icon">
+            <IoIosArrowBack />
+          </span>
+          Back
+        </button>
+      </Link>
+
+      <h2>Order details</h2>
+      <div className="order-details-container">
+        <div className="product-img">
+          <img
+            src="https://m.media-amazon.com/images/I/61GupjuEzDL._AC_SX466_.jpg"
+            alt="microwave"
+          />
+        </div>
+
+        <div className="product-details-container">
+          <div className="products-description-details">
+            <span className="product-name">
+              Microwave {orderDetailsObj?.products?.name}
+            </span>
+            <p className="product-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </p>
+            <p className="product-quantity">Qty 1</p>
+          </div>
+
+          <div className="customer-details-container">
+            <div className="customer-name">
+              <p>Customer Name:</p>
+              {orderDetailsObj?.member?.fullName}
+            </div>
+
+            <div className="customer-email">
+              <p>Email:</p>
+              {orderDetailsObj?.member?.email}
+            </div>
+
+            <div className="customer-address">
+              <p>Address: </p>
+              {orderDetailsObj?.member?.address},{orderDetailsObj?.member?.city}
+              ,{orderDetailsObj?.member?.postcode},
+              {orderDetailsObj?.member?.country}
+            </div>
+
+            <div className="customer-number">
+              <p>Phone Number: </p>
+              {orderDetailsObj?.member?.phoneNumber}
+            </div>
+          </div>
         </div>
       </div>
-      <div>
-        <div>Customer Name: {orderDetailsObj?.member?.fullName}</div>
-        <div>Email: {orderDetailsObj?.member?.email}</div>
-        <div>
-          Address: {orderDetailsObj?.member?.address},
-          {orderDetailsObj?.member?.city},{orderDetailsObj?.member?.postcode},
-          {orderDetailsObj?.member?.country}
-        </div>
-        <div>Phone Number: {orderDetailsObj?.member?.phoneNumber}</div>
-      </div>
-      <div>order status update buttons</div>
-      <div>
-        <span>Date:</span>
-        <span>Date:</span>
+
+      <div className="form-container">
+        <form className="form">
+          <label className="container">Delivered</label>
+          <input type="checkbox" className="checkbox" />
+
+          <label className="container">Picked up</label>
+          <input type="checkbox" className="checkbox" />
+
+          <label className="container">Waiting list</label>
+          <input type="checkbox" className="checkbox" />
+
+          <label className="container">Cancelled</label>
+          <input type="checkbox" className="checkbox" />
+        </form>
       </div>
     </>
   );
