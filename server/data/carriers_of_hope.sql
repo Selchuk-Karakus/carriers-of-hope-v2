1c2265e9586f340ee7f35e96737dd60e11d92d1a
Hamra <hamra.khan.b@gmail.com>
2022-07-23 01:26:10 +0100

    added member to database for admin login

--- server/data/carriers_of_hope.sql
+++ server/data/carriers_of_hope.sql
@@ -62,6 +62,7 @@ INSERT INTO members (first_name, last_name, email, address, city, postcode, coun
          ('Sami','Hakim','s.hakim@johnsmith.org','15 New Road','London','L10 2AB','UK', 56789012345, 'samihakim', TRUE),
          ('Hedyeh','Etemadi','h.etemedi@johnsmith.org','16 New Road','Manchester','L10 2AB','UK', 67890123456, 'hedyehetemaddi', TRUE),
          ('Olga','Olga','o.olga@johnsmith.org','17 New Road','London','L10 2AB','UK', 78901234567, 'olgaolga', TRUE);
+         ('george', 'Karakus', 'h@kklllkk8.org', '25 New Road', 'London', 'L10 2AB', 'UK', 23456789101, '$2b$10$1mfWjqXDwpPAbwVcvfs8IumGSzUwubsvPYpn76Hm6b1os/WssVrc2', TRUE);
 
 INSERT INTO products (product_name, category_name),
     VALUES  ('Single bed frame', 'Furniture', true),
