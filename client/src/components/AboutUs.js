5da1b46abe240101cb57dc7cf5b4b867cf71d6ca
mahri09 <mahriaa88@gmail.com>
2022-07-26 04:34:13 +0100

    changed style home

---
+++ client/src/components/AboutUs.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+  <div className="about-us">
+   <div className="image-container">
+      <img src="./images/aboutUs.jpg" alt="about-us image"/>
+   </div>
+   <div className="about-text">
+      <p>Coventry is a place where Asylum Seekers, Refugees and Migrant Families are welcomed and inspired to rebuild their lives for a better future.</p>
+      <p>Our Mission is to provide a warm welcome, practical support and care, to Asylum Seekers, Refugees and Migrant families in need
+      To befriend and empower individuals to be active citizens
+      </p>
+      <a href="https://carriersofhope.org.uk/about-us/">Find out More</a>
+   </div>
+  </div>
+  );
+}
+
+export default AboutUs;
